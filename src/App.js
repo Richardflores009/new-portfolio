e7ee572c7080e9f110665ed999bb9822b04aef54
Dick Flowers <dickflowers@DickMiniPro.local>
2021-01-31 09:18:15 -0700

    added project components and connected them to the app js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-
 import './App.css';
+import Home from './components/Home'
+import Contact from './components/Contact'
+import Portfolio from './components/Portfolio'
+import About from './components/About'
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 
 function App() {
-
-
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Me</Link>
+            </li>
+            <li>
+              <Link to="/portfolio">Portfolio</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Me</Link>
+            </li>
+          </ul>
+        </nav>
+
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />  
+          </Route>          
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
